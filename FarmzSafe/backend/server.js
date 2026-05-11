832ff8dad5c05d36dabf249016c6c058e6b0593a
Mayra123-netizen <maryam.olanlege@gmail.com>
2026-05-11 18:14:28 +0100

    first commit
    First FarmzSafe commit.

---
+++ FarmzSafe/backend/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const connectDB = require("./config/database");
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.json());
+
+
+connectDB(); 
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
